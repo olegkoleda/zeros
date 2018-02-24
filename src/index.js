8bd45d6645f19dbff423bebffa9e464342df303e
Oleg <olegator2510@mail.ru>
2018-02-24 21:23:12 +0200

    zeroscount done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  var zerosCount = 0;
+
+  for ( var i = 5; number / i >= 1; i *= 5 ) {
+    zerosCount += ~~(number / i);
+  }
+  return zerosCount;
 }
